d8779456e0a7675e9d9c3cb68745c904da8c024e
ShijiMi-Soup <takahiro.zt899@gmail.com>
2024-07-14 15:11:44 +0900

    Add material ui

---
+++ src/pages/waveform-viewer/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export const WaveformViewerPage = () => {
+  return (
+    <div>
+      <Typography variant="h2">Waveform Viewer</Typography>
+    </div>
+  );
+};
---
+++ src/theme/theme.ts
@@ -0,0 +1,9 @@
+import { cyan } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+export const lightTheme = createTheme({
+  palette: {
+    primary: cyan,
+    mode: "light",
+  },
+});
